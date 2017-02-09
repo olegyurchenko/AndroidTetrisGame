cb8083e7650d879f2f4664656733b1bc4d29f9cb
olegyurchenko <oleg.yurchenko@gmail.com>
2017-02-09 13:33:07 +0200

    Work with help activity

--- app/src/main/java/ua/probe/oleg/tetrisgamecollection/HelpActivity.java
+++ app/src/main/java/ua/probe/oleg/tetrisgamecollection/HelpActivity.java
@@ -3,6 +3,7 @@ package ua.probe.oleg.tetrisgamecollection;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.TabHost;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -13,5 +14,34 @@ public class HelpActivity extends AppCompatActivity {
 
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
+
+    TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
+    // инициализация
+    tabHost.setup();
+
+    TabHost.TabSpec tabSpec;
+
+    tabSpec = tabHost.newTabSpec("tag1");
+    tabSpec.setIndicator(getString(R.string.about));
+    tabSpec.setContent(R.id.text_abouut);
+    tabHost.addTab(tabSpec);
+
+    tabSpec = tabHost.newTabSpec("tag2");
+    tabSpec.setIndicator(getString(R.string.action_tetris));
+    tabSpec.setContent(R.id.text_tetris);
+    tabHost.addTab(tabSpec);
+
+    tabSpec = tabHost.newTabSpec("tag3");
+    tabSpec.setIndicator(getString(R.string.action_columnus));
+    tabSpec.setContent(R.id.text_columnus);
+    tabHost.addTab(tabSpec);
+
+    tabSpec = tabHost.newTabSpec("tag4");
+    tabSpec.setIndicator(getString(R.string.action_color_balls));
+    tabSpec.setContent(R.id.text_color_balls);
+    tabHost.addTab(tabSpec);
+
+    // вторая вкладка будет выбрана по умолчанию
+    tabHost.setCurrentTabByTag("tag1");
   }
 }
