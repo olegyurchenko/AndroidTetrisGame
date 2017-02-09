e076e7b0729974de8d2a6199be10037f2e56c5b4
oleg <oleg.yurchenko@gmail.com>
2017-02-09 20:33:02 +0200

    Work with help and version

--- app/src/main/java/ua/probe/oleg/tetrisgamecollection/HelpActivity.java
+++ app/src/main/java/ua/probe/oleg/tetrisgamecollection/HelpActivity.java
@@ -1,15 +1,25 @@
 package ua.probe.oleg.tetrisgamecollection;
 
 import android.content.pm.ApplicationInfo;
+import android.content.res.AssetManager;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.TabHost;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.text.DateFormat;
 import java.util.Locale;
 
@@ -56,7 +66,7 @@ public class HelpActivity extends AppCompatActivity {
 
 
 
-    TextView aboutText = (TextView) findViewById(R.id.text_abouut);
+    TextView aboutTextView = (TextView) findViewById(R.id.text_abouut);
     //int versionCode = BuildConfig.VERSION_CODE;
     String versionName = BuildConfig.VERSION_NAME;
 
@@ -64,8 +74,12 @@ public class HelpActivity extends AppCompatActivity {
       versionName,
       //versionCode,
       getAppTimeStamp());
-    aboutText.setText(versionText);
 
+    String aboutText = String.format(Locale.getDefault(),"<p>%s</p><p>%s</p>",
+      readAboutText(),
+      versionText);
+
+    aboutTextView.setText(Html.fromHtml(aboutText));
 
     WebView wv = (WebView)findViewById(R.id.view_tetris);
     wv.loadUrl(getString(R.string.tetris_html_help_file));
@@ -99,4 +113,25 @@ public class HelpActivity extends AppCompatActivity {
 
   }
     /*============================================================*/
+  public String readAboutText() {
+    StringBuilder result = new StringBuilder();
+    try {
+      AssetManager am = getAssets();
+      InputStream is = am.open("about.txt");
+      BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+
+      String line;
+      boolean flag = false;
+      while ((line = reader.readLine()) != null) {
+        result.append(flag ? "\n" : "").append(line);
+        flag = true;
+      }
+    }
+
+    catch(IOException e) {
+      Log.e("HelpAbout", e.getLocalizedMessage());
+    }
+
+    return result.toString();
+  }
 }
